3608a3606f24090ddfeffca9d2fcf3739fea7e16
jaingunavardhan <jaingunavardhan@gmail.com>
2024-03-19 00:31:20 +0530

    Updated with comments for better understanding

--- util/database.js
+++ util/database.js
@@ -1,10 +1,14 @@
+//Importing mysql2
 const mysql = require('mysql2');
 
+//Creating a Connection Pool so that we don't exhaust the connextion OR
+// We don't have to create a new connection everytime we have read/write operations
 const pool = mysql.createPool({
-    host:"localhost",
-    database:"node_db",
-    user:"root",
-    password:"guna"
+    host:"localhost",       //Hostname
+    database:"node_db",     //Database name
+    user:"root",            //Username
+    password:"guna"         //Password
 });
 
+//Exporting this DB connection pool so that it can be used by the importing files
 module.exports = pool.promise();
